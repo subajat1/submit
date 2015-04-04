cdcf4aa2320f88a2589a5339b80ecbbfdfb2b768
subajato <bayumunajat@outlook.com>
2015-04-05 01:31:15 +0700

    improved IQ
    improved IQ
    many bugs
    running tested
    sleepy I am

--- jp/co/worksap/global/ImmutableQueue.java
+++ jp/co/worksap/global/ImmutableQueue.java
@@ -2,53 +2,64 @@ package jp.co.worksap.global;
 
 import java.util.NoSuchElementException;
 
+/// ==================================================================
+// 
+// inspired from Vivek Mangla
+/// ==================================================================
 public class ImmutableQueue<E> {
 
-	List FRONT = null, REAR = null;
+	List<E> FRONT = null, REAR = null;
 	int SIZE = 0;
 	ImmutableQueue<E> immutableQueue = null;
-	
-	public ImmutableQueue(){
-		
+
+	@SuppressWarnings("rawtypes")
+	private List helper;
+
+	public ImmutableQueue() {
+		// modify this constructor if necessary, but do not remove default
+		// constructor
 	}
-	
+
 	// add other constructors if necessary
-	
-	public ImmutableQueue<E> enqueue(E e){
-		return null;
-	}	
-	
-	public ImmutableQueue<E> dequeue(){
-		if((this.FRONT == null)||(this.REAR.NEXT == this.FRONT)){
+
+	public ImmutableQueue<E> enqueue(E e) {
+		this.helper = new List();
+
+		if ((this.FRONT == null) || (this.REAR.NEXT == this.FRONT)) {
+			this.immutableQueue.REAR = helper;
+			this.immutableQueue.FRONT = helper;
+		} else if (this.REAR.NEXT == null) {
+			this.immutableQueue.FRONT = this.FRONT;
+			this.immutableQueue.REAR = this.helper;
+			this.REAR.NEXT = this.helper;
+		}
+		return this.immutableQueue;
+	}
+
+	public ImmutableQueue<E> dequeue() {
+		if ((this.FRONT == null) || (this.REAR.NEXT == this.FRONT)) {
 			this.FRONT = null;
 			this.REAR = null;
-            
-            throw new NoSuchElementException();
-            /** <<YOU_MAY_HANDLE_THIS_EXCEPTION_ON_YOUR_OWN>> **/
-        }
-       
-        this.immutableQueue = new ImmutableQueue<E>();
-        this.immutableQueue.FRONT = this.FRONT.NEXT;
-        this.immutableQueue.REAR = this.REAR;
-        this.immutableQueue.SIZE=SIZE-1;
-       
-        return this.immutableQueue;
+		}
+
+		this.immutableQueue = new ImmutableQueue<E>();
+		this.immutableQueue.FRONT = this.FRONT.NEXT;
+		this.immutableQueue.REAR = this.REAR;
+		this.immutableQueue.SIZE = SIZE - 1;
+
+		return this.immutableQueue;
 	}
-	
-	public E peek(){
-		return null;
+
+	public E peek() {
+		return (E) this.FRONT.object;
 	}
-	
-	public int size(){
-		return -1;
+
+	public int size() {
+		return this.SIZE;
 	}
-		
-	
-	
-	class List<E>{
-	    
-	    
-	    E object;/**<<Node Data Part Can Contain Any Object>>**/
-	    List NEXT = null;/***<<Reference to next Node>>**/
+
+	public class List<e> {
+		e object;
+		List<e> NEXT = null;
 	}
 }
